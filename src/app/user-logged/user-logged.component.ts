5858a505c0f53b2a0b31f6ce4063105276ee955c
q1e123 <robertnicolaesolca@gmail.com>
2022-05-11 11:40:10 +0300

    added userloged username

--- src/app/user-logged/user-logged.component.ts
+++ src/app/user-logged/user-logged.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoggedComponent implements OnInit {
 
-  constructor() { }
+  username = ''
+  constructor() { 
+    this.username = localStorage.getItem('currentUser') || '';
+  }
 
   ngOnInit(): void {
   }
